ed7c421629437653ce3e609d5f8944f8553f06f4
Lisias <me@lisias.net>
2021-02-20 21:04:36 -0300

    Adding Versioning. Adding KSPe Installment check.

--- Source/ConnectedLivingSpace/Properties/AssemblyInfo.cs
+++ Source/ConnectedLivingSpace/Properties/AssemblyInfo.cs
@@ -4,14 +4,20 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConnectedLivingSpace")]
+[assembly: AssemblyTitle("ConnectedLivingSpace /L Unofficial")]
 [assembly: AssemblyDescription("KSP Plugin to detect internally connected living spaces")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("PWB")]
-[assembly: AssemblyProduct("ConnectedLivingSpace")]
-[assembly: AssemblyCopyright("Copyright ©  2014, 2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany(KSPe.LegalMamboJambo.Company)]
+[assembly: AssemblyProduct(KSPe.LegalMamboJambo.Product)]
+[assembly: AssemblyCopyright(KSPe.LegalMamboJambo.Copyright)]
+[assembly: AssemblyTrademark(KSPe.LegalMamboJambo.Trademark)]
 [assembly: AssemblyCulture("")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,14 +27,12 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("39ece0ec-3186-45d4-9cfd-bac6548c3f72")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.6")]
 //[assembly: AssemblyFileVersion("2.0.0.3")]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: AssemblyVersion(ConnectedLivingSpace.Version.Number)]
+[assembly: AssemblyFileVersion(ConnectedLivingSpace.Version.Number)]
+[assembly: KSPAssembly("ConnectedLivingSpace", KSPe.Version.major, KSPe.Version.minor)]
+[assembly: KSPAssemblyDependency("KSPe", 2, 2)]
+[assembly: KSPAssemblyDependency("KSPe.UI", 2, 2)]
